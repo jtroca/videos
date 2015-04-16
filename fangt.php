24dc79a9932da6720219df25af9260aca9075123
jtroca <john.roca@intechsive.com>
2015-04-16 17:31:34 +0800

    jw player testing

---
+++ fangt.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script src="http://jwpsrv.com/library/8QcjWuAOEeSN8hJtO5t17w.js"></script>
+        <script>jwplayer.key="oFywHfaHdTFe8gUoCpClkiZBYLtFouQ7PD7m/g==";</script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-4">
+                </div>
+                <div class="col-lg-4">
+                    <div class="row">
+                        <div id='playerrlEgZtJJJmCl'></div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                </div>
+            </div>
+        </div>
+        <script type='text/javascript'>
+            jwplayer('playerrlEgZtJJJmCl').setup({
+                    flashplayer : "/jwplayer/jwplayer.flash.swf",
+                    autostart: 'true',
+                    sources : [
+                            { file: "http://fuckanewgirltonight.com/video/sales22fast.mp4" },
+                            { file: "http://fuckanewgirltonight.com/video/sales17_48k.webm" },
+                     ]
+                });
+        </script>
+    </body>
+</html>
